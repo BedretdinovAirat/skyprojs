13d7db7ce342aba61e81138596f92fb7834fb573
BedretdinovAirat <bedretdinov-a@mail.ru>
2023-02-19 21:59:28 +0300

    reales hw2

---
+++ hw-2.js
@@ -0,0 +1,61 @@
+// // 1 mission
+// let a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
+// // 2 mission
+// let iPhone = "29062007";
+// alert(iPhone);
+// // 3 mission
+// let nameJs = "BrendanEich";
+// alert(nameJs);
+// // 4 mission
+// let num_1 = 10;
+// let num_2 = 2;
+// let sum = num_1 + num_2;
+// let raz = num_1 - num_2;
+// let pro = num_1 * num_2;
+// let del = num_1 / num_2;
+// alert(sum); /* доделать 4 задание*/
+// alert(raz);
+// alert(pro);
+// alert(del);
+// // 5 mission 
+// let num_3 = 2;
+// let num_4 = 5;
+// let result = num_3 ** num_4;
+// alert(result);
+// // 6 misssion
+// let x = 9;
+// let y = 2;
+// let result_1 = x % y;
+// alert(result_1);
+// // 7 mission
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+// alert(num);
+// // 8 mission 
+// let age = prompt("Сколько вам лет?");
+// alert(age);
+// 9 mission 
+// let user = {
+//     name: "Airat",
+//     age: 20,
+//     isAdmin: true,
+//     age: 100 // 9.2 mission
+// };
+// //  9.1 mission
+// user.property = "city of residence";
+// // 9.3 mission
+// delete property("city of residence");
+// // 9.4 mission
+// let info = prompt("Какую информацию хотите узнать о пользователе?");
+// alert(`info`);
+// 10 mission
+let firstName = prompt("Назовите ваше имя");;
+alert("Привет, " + `${firstName}` + "!");
